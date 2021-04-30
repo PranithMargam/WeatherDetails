04f5e12d2321c613e4114886f8f955075b2e656e
Pranith Margam <margampranith@gmail.com>
2021-04-30 19:09:04 +0530

    Initial Commit

---
+++ WeatherDetails/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WeatherDetails
+//
+//  Created by Pranith Margam on 30/04/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
